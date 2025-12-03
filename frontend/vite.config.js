91b40ef29a5be979743fea5c9045091d7bde4947
fullstackdevarchon <fullstackdev.archon@gmail.com>
2025-12-03 20:05:32 +0530

    ren19

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/",  // Required for Render static hosting
+  base: "/",  // Important for Render
 
   build: {
     outDir: "dist",
@@ -12,12 +12,6 @@ export default defineConfig({
 
   define: {
     __SERVER_HOST__: JSON.stringify(process.env.VITE_API_URL),
-    __DEFINES__: { SERVER_HOST: process.env.VITE_API_URL },
-    __HMR_CONFIG_NAME__: JSON.stringify(process.env.VITE_HMR_CONFIG_NAME || ""),
-    __BASE__: JSON.stringify(process.env.BASE_URL || ""),
-    __HMR_PROTOCOL__: JSON.stringify(process.env.VITE_HMR_PROTOCOL || ""),
-    __HMR_PORT__: JSON.stringify(process.env.VITE_HMR_PORT || ""),
-    __HMR_HOSTNAME__: JSON.stringify(process.env.VITE_HMR_HOSTNAME || "")
   },
 
   server: {
