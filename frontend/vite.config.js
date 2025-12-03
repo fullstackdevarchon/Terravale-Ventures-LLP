1e6b664326d5c653080e82e188cc58b0726957d0
fullstackdevarchon <fullstackdev.archon@gmail.com>
2025-12-03 20:21:14 +0530

    fresh build: removed __DEFINES__

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/",  // Important for Render
+  base: "/",
 
   build: {
     outDir: "dist",
-  },
-
-  define: {
-    __SERVER_HOST__: JSON.stringify(process.env.VITE_API_URL),
   },
 
   server: {
