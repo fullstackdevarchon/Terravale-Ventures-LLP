35e2efa6a842dc62b92e772c49a541ea1fc8cb45
fullstackdevarchon <fullstackdev.archon@gmail.com>
2025-12-03 12:20:29 +0530

    ren10

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/", // Required for Render
+  base: "/",  // Required for Render static hosting
 
   build: {
     outDir: "dist",
   },
 
+  define: {
+    __DEFINES__: JSON.stringify({ SERVER_HOST: process.env.VITE_API_URL })
+  },
+
   server: {
     host: true,
     port: 5173,
-  }
+  },
 });
